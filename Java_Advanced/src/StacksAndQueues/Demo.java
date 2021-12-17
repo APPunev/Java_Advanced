bf7b6f6946db47764f98f90fcb7c078862877e33
atanaspunev <punev90@gmail.com>
2021-12-17 19:32:43 +0200

    Added more exercises and solution.

--- Java_Advanced/src/StacksAndQueues/Demo.java
+++ Java_Advanced/src/StacksAndQueues/Demo.java
@@ -1,30 +1,69 @@
 package StacksAndQueues;
 
+
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] token = input.split("\\s+");
 
-        int n = Integer.parseInt(scanner.nextLine());
+        ArrayDeque<String> stack = new ArrayDeque<>();
+        ArrayDeque<String> queue = new ArrayDeque<>();
 
-        for (int j = 1; j <= n; j++) {
-            int num = j;
-            boolean flag = false;
-            for (int i = 2; i <= num / 2; ++i) {
-                // condition for nonprime number
-                if (num % i == 0) {
-                    flag = true;
-                    break;
+        for (String c:token) {
+
+            if (isLetterOrDigit(c)) {
+                queue.offer(c);
+            }else if (c.equals("+") || c.equals("-") || c.equals("*") || c.equals("/")) {
+                while(!stack.isEmpty() && getPrecedence(c) <= getPrecedence(stack.peek())){
+                    queue.offer(stack.pop());
+                }
+                stack.push(c);
+            }else if (c.equals(")")) {
+                while(!stack.isEmpty() && !stack.peek().equals("(")){
+                    queue.offer(stack.pop());
+                }
+                stack.pop();
+            }else if (c.equals("(")) {
+                stack.push(c);
+            }
+        }
+        while (!stack.isEmpty()) {
+            if (stack.peek().equals("("))
+                System.out.println("This expression is invalid");
+            queue.offer(stack.pop());
+        }
+        queue.forEach(e -> System.out.print(e + " "));
+    }
+
+    private static boolean isLetterOrDigit(String c) {
+        if (c.length()> 1) {
+            for (int i = 0; i < c.length(); i++) {
+                char a = c.charAt(i);
+                if (!Character.isLetterOrDigit(a)) {
+                    return false;
                 }
             }
-
-            if (!flag)
-                System.out.println(num + " is a prime number.");
-            else
-                System.out.println(num + " is not a prime number.");
-
+        }else{
+            char b = c.charAt(0);
+            if (!Character.isLetterOrDigit(b)) {
+                return false;
+            }
         }
+        return true;
+    }
 
+    private static int getPrecedence(String ch) {
+        if (ch.equals("+") || ch.equals("-"))
+            return 1;
+        else if (ch.equals("*") || ch.equals("/"))
+            return 2;
+        else if (ch.equals("^"))
+            return 3;
+        else
+            return -1;
     }
 }
