8d08bf411c9c8029daae7d236d74d2907a79a679
atanaspunev <punev90@gmail.com>
2022-01-11 10:58:55 +0200

    Modifications made to the classes

--- Java_Advanced/src/StacksAndQueues/EXR_PoisonousPlants.java
+++ Java_Advanced/src/StacksAndQueues/EXR_PoisonousPlants.java
@@ -4,7 +4,7 @@ import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class PoisonousPlants {
+public class EXR_PoisonousPlants {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
